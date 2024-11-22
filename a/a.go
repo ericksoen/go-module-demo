9f2bdde2ad55dcfd1e3f7c7a6620edb93809e5c3
Michael Ericksen <mericksen@imohealth.com>
2024-11-21 22:16:41 -0600

    Add package a

---
+++ a/a.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+  "github.com/ericksoen/go-module-demo/b"
+  "fmt"
+)
+
+const Name = b.Name
+
+func main() {
+  fmt.Println(Name)
+}
---
+++ a/go.mod
@@ -0,0 +1,5 @@
+module github.com/ericksoen/go-modules-demo/a
+
+go 1.22.2
+
+require github.com/ericksoen/go-module-demo/b v0.1.1
